ec6aafa75b51964557b3c6673a38d9576c4638f6
Michał Szymański <michalszymanskyy@gmail.com>
2025-03-19 23:54:06 +0100

    Podstawy Sztucznej Inteligencji - Beta 8 - Zabawne Podpisy

--- Main.py
+++ Main.py
@@ -1,9 +1,11 @@
 import subprocess
+
 import time
+
 
 # Powiadomienie o rozpoczęciu działania SI
 
-print("SI zaczęło działać...")
+print("Maszyna w Ogniu...")
 
 
 # Rozpoczęcie licznika czasu
@@ -12,16 +14,26 @@ start_time = time.time()
 
 
 # Uruchomienie skryptu EdycjaDanych.py
+
 print("Uruchamianie EdycjaDanych.py...")
+
 subprocess.run(["python", "EdycjaDanych.py"], check=True)
 
+
 # Uruchomienie skryptu PyTorch.py
+
 print("Uruchamianie PyTorch.py...")
+
 subprocess.run(["python", "PyTorch.py"], check=True)
 
+
 # Zakończenie licznika czasu
+
 end_time = time.time()
+
 elapsed_time = end_time - start_time
 
+
 # Wyświetlenie czasu działania
-print(f"SI zakończyło działanie. Całkowity czas: {elapsed_time:.2f} sekundy.")
+
+print(f"Maszyna na OIOMie! Całkowity Czas: {elapsed_time:.2f} Sekundy.")
